168dc9e69c6557f968b0126654ba9a38d826f0c6
Banu Ağgün <banuaggun@outlook.com>
2026-01-14 22:23:49 +0300

    The structure's functionality was confirmed through a sample demonstration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setInvoices } from "./features/invoiceSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const invoices = useSelector((state) => state.invoices.invoices);
+
+  useEffect(() => {
+    if (!localStorage.getItem("invoices")) {
+      fetch("/db/data.json")
+        .then((res) => res.json())
+        .then((data) => dispatch(setInvoices(data.invoices)));
+    }
+  }, [dispatch]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Invoice App</h1>
+      <ul>
+        {invoices.map((inv) => (
+          <li key={inv.id}>
+            <strong>{inv.id}</strong> - {inv.clientName} - {inv.total}$
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
