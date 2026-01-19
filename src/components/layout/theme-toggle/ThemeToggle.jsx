a64c279eabfc2fe5cc8b674fc6b622e691e3f8b2
Banu Ağgün <banuaggun@outlook.com>
2026-01-19 22:18:41 +0300

    The Header Area toggle button has been edited.

--- src/components/layout/theme-toggle/ThemeToggle.jsx
+++ src/components/layout/theme-toggle/ThemeToggle.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleTheme } from "../../../features/themeSlice";
+import { toggleTheme } from "../../../features/themeSlice"; 
+import './theme-toggle.css'; 
 
 function ThemeToggle() {
     const dispatch = useDispatch();
     const theme = useSelector((state) => state.theme.mode);
 
     return (
-        <button onClick={() => dispatch(toggleTheme())}>
-            {theme === "light" ? "🌙 Dark" : "☀️ Light"}
+        <button className="theme-toggle-button" onClick={() => dispatch(toggleTheme())}>
+            {theme === "light" ? (
+                <>
+                    {/* Moon Icon */}
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        viewBox="0 0 256 256"
+                    >
+                        <path d="M248,160a8,8,0,0,1-8,8H16a8,8,0,0,1,0-16H56.45a73.54,73.54,0,0,1-.45-8,72,72,0,0,1,144,0,73.54,73.54,0,0,1-.45,8H240A8,8,0,0,1,248,160Zm-40,32H48a8,8,0,0,0,0,16H208a8,8,0,0,0,0-16ZM80.84,59.58a8,8,0,0,0,14.32-7.16l-8-16a8,8,0,0,0-14.32,7.16ZM20.42,103.16l16,8a8,8,0,1,0,7.16-14.31l-16-8a8,8,0,1,0-7.16,14.31ZM216,112a8,8,0,0,0,3.57-.84l16-8a8,8,0,1,0-7.16-14.31l-16,8A8,8,0,0,0,216,112ZM164.42,63.16a8,8,0,0,0,10.74-3.58l8-16a8,8,0,0,0-14.32-7.16l-8,16A8,8,0,0,0,164.42,63.16Z"></path>
+                    </svg>
+                    <span>Light</span>
+                </>
+            ) : (
+                <>
+                    {/* Sun Icon */}
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        viewBox="0 0 256 256"
+                    >
+                        <path d="M240,96a8,8,0,0,1-8,8H216v16a8,8,0,0,1-16,0V104H184a8,8,0,0,1,0-16h16V72a8,8,0,0,1,16,0V88h16A8,8,0,0,1,240,96ZM144,56h8v8a8,8,0,0,0,16,0V56h8a8,8,0,0,0,0-16h-8V32a8,8,0,0,0-16,0v8h-8a8,8,0,0,0,0,16Zm65.14,94.33A88.07,88.07,0,0,1,105.67,46.86a8,8,0,0,0-10.6-9.06A96,96,0,1,0,218.2,160.93a8,8,0,0,0-9.06-10.6Z"></path>
+                    </svg>
+                    <span>Dark</span>
+                </>
+            )}
         </button>
     );
 }
