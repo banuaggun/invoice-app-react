a54af84aba40b32011703e35e080bf5208275f7f
Banu Ağgün <banuaggun@outlook.com>
2026-01-21 21:26:47 +0300

    Positioning was adjusted. - 2

--- src/components/invoice-item/InvoiceItem.jsx
+++ src/components/invoice-item/InvoiceItem.jsx
@@ -6,15 +6,29 @@ function InvoiceItem({ invoice }) {
     const navigate = useNavigate();
 
     return (
-        <div
-            style={{ border: "1px solid #ccc", margin: "10px", padding: "10px", cursor: "pointer" }}
-            onClick={() => navigate(`/invoice/${invoice.id}`)} // 👈 yönlendirme
-        >
-            <h3>{invoice.id}</h3>
+        <div className="invoice-card" onClick={() => navigate(`/invoice/${invoice.id}`)} 
+        > 
+        <div className="row-1">
+            <h3>{invoice.id}</h3> 
+            <p>Details <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="var(--text-color)" viewBox="0 0 256 256"><path d="M184.49,136.49l-80,80a12,12,0,0,1-17-17L159,128,87.51,56.49a12,12,0,1,1,17-17l80,80A12,12,0,0,1,184.49,136.49Z"></path></svg></p>
+        </div> 
+
+        <div className="row-2">
+            <p> {invoice.clientName}</p>
             <p>{invoice.createdAt}</p>
-            <p><strong>Client:</strong> {invoice.clientName}</p>
-            <p><strong>Total:</strong> ${invoice.total}</p>
-            <p><strong>Status:</strong> {invoice.status}</p>
+            
+        </div> 
+
+        <div className="row-3">
+  <p>${invoice.total}</p>
+  <div className={`status-badge ${invoice.status.toLowerCase()}`}>
+    <span className="status-dot"></span>
+    <span className="status-text">{invoice.status}</span>
+  </div>
+</div>
+
+            
+            
         </div>
     );
 }
