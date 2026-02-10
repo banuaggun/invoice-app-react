8c4b8e71265b8ef18bb6fc498a5ddd88848cf6d3
Banu Ağgün <banuaggun@outlook.com>
2026-02-10 23:03:13 +0300

    style: item added

--- src/pages/invoice-detail/InvoiceDetail.jsx
+++ src/pages/invoice-detail/InvoiceDetail.jsx
@@ -27,18 +27,21 @@ const InvoiceDetail = () => {
   return (
     <div className="invoice-detail">
       <div className="invoice-detail-topbar">
-        <button className="back-btn" onClick={() => navigate(-1)}>
-          ← Back
+        <button className="detail-button back-btn" onClick={() => navigate(-1)}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M205.66,202.34a8,8,0,0,1-11.32,11.32l-80-80a8,8,0,0,1,0-11.32l80-80a8,8,0,0,1,11.32,11.32L131.31,128ZM51.31,128l74.35-74.34a8,8,0,0,0-11.32-11.32l-80,80a8,8,0,0,0,0,11.32l80,80a8,8,0,0,0,11.32-11.32Z"></path></svg> 
+          <span>Back</span>
         </button>
         <div className="actions">
           <button
-            className="edit-btn"
+            className="detail-button edit-btn"
             onClick={() => navigate(`/invoice/${invoice.id}/edit`)}>
-            Edit
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M227.32,73.37,182.63,28.69a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H216a8,8,0,0,0,0-16H115.32l112-112A16,16,0,0,0,227.32,73.37ZM136,75.31,152.69,92,68,176.69,51.31,160ZM48,208V179.31L76.69,208Zm48-3.31L79.32,188,164,103.31,180.69,120Zm96-96L147.32,64l24-24L216,84.69Z"></path></svg> 
+            <span>Edit</span>
           </button>
 
-          <button className="delete-btn" onClick={() => setIsModalOpen(true)}>
-            Delete
+          <button className="detail-button delete-btn" onClick={() => setIsModalOpen(true)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path></svg> 
+            <span>Delete</span>
           </button>
         </div>
       </div>
