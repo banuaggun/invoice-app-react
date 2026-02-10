189166bef9e4e752400f1d131a1f011ab877ec8e
Banu Ağgün <banuaggun@outlook.com>
2026-02-11 01:28:08 +0300

    style: minor style changes (item positioning)

--- src/components/invoice-item/InvoiceItem.jsx
+++ src/components/invoice-item/InvoiceItem.jsx
@@ -20,7 +20,7 @@ const formattedDate = formatDate(displayDate, "tr-TR");
               <td data-label="date">{formattedDate}</td> 
               <td data-label="total">${invoice.total}</td> 
               <td className="more-btn" onClick={() => navigate(`/invoice/${invoice.id}`)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#924217" viewBox="0 0 256 256"><path d="M141.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L124.69,128,50.34,53.66A8,8,0,0,1,61.66,42.34l80,80A8,8,0,0,1,141.66,133.66Zm80-11.32-80-80a8,8,0,0,0-11.32,11.32L204.69,128l-74.35,74.34a8,8,0,0,0,11.32,11.32l80-80A8,8,0,0,0,221.66,122.34Z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="var(--text-color)" viewBox="0 0 256 256"><path d="M141.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L124.69,128,50.34,53.66A8,8,0,0,1,61.66,42.34l80,80A8,8,0,0,1,141.66,133.66Zm80-11.32-80-80a8,8,0,0,0-11.32,11.32L204.69,128l-74.35,74.34a8,8,0,0,0,11.32,11.32l80-80A8,8,0,0,0,221.66,122.34Z"></path></svg>
                 <span>More</span> 
               </td>
             </tr>
