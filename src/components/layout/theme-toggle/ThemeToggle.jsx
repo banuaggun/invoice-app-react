c3a69d23ec72b2c3ec8b3b0e9b3e1a5e64b88ed8
Banu Ağgün <banuaggun@outlook.com>
2026-02-11 13:30:25 +0300

    style: update side nav layout

--- src/components/layout/theme-toggle/ThemeToggle.jsx
+++ src/components/layout/theme-toggle/ThemeToggle.jsx
@@ -14,8 +14,8 @@ function ThemeToggle() {
                     {/* Moon Icon */}
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
+                        width="32"
+                        height="32"
                         fill="currentColor"
                         viewBox="0 0 256 256"
                     >
@@ -28,8 +28,8 @@ function ThemeToggle() {
                     {/* Sun Icon */}
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
+                        width="32"
+                        height="32"
                         fill="currentColor"
                         viewBox="0 0 256 256"
                     >
