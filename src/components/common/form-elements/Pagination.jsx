f04bf96b16c5b9b5ff0fa9c487e949a0f658a6c0
Banu Ağgün <banuaggun@outlook.com>
2026-03-16 16:43:53 +0300

    feat(pagination): align forward button right on step 1, add back button left on subsequent steps

--- src/components/common/form-elements/Pagination.jsx
+++ src/components/common/form-elements/Pagination.jsx
@@ -1,33 +1,54 @@
-import React from 'react' 
-import './form-elements.css'; 
-import BackIcon from '../../../assets/icons/BackIcon'; 
-import ForwardIcon from '../../../assets/icons/ForwardIcon';
+import React from "react";
+import "./form-elements.css";
+import BackIcon from "../../../assets/icons/BackIcon";
+import ForwardIcon from "../../../assets/icons/ForwardIcon";
 
 const Pagination = ({ step, setStep, showPreview, setShowPreview }) => {
   return (
     <div className="pagination">
-      {step > 1 && (
-        <button className='btn-back cta-back' type="button" onClick={() => setStep(step - 1)}>
-          <BackIcon />
-        </button>
+      {step === 1 && (
+        <div className="right">
+          <button
+            className="btn-forward cta-back"
+            type="button"
+            onClick={() => setStep(step + 1)}>
+            <ForwardIcon />
+          </button>
+        </div>
       )}
-      {step < 5 && (
-        <button className='btn-forward cta-back' type="button" onClick={() => setStep(step + 1)}>
-          <ForwardIcon />
-        </button>
+
+      {step > 1 && step < 5 && (
+        <>
+          <button
+            className="btn-back cta-back"
+            type="button"
+            onClick={() => setStep(step - 1)}>
+            <BackIcon />
+          </button>
+          <button
+            className="btn-forward cta-back"
+            type="button"
+            onClick={() => setStep(step + 1)}>
+            <ForwardIcon />
+          </button>
+        </>
       )}
+
       {step === 5 && (
-        <button type="button" onClick={() => setShowPreview(!showPreview)}>
-          {showPreview ? "Preview Close" : "Preview"}
-        </button>
+        <>
+          <button
+            className="btn-back cta-back"
+            type="button"
+            onClick={() => setStep(step - 1)}>
+            <BackIcon />
+          </button>
+          <button type="button" onClick={() => setShowPreview(!showPreview)}>
+            {showPreview ? "Preview Close" : "Preview"}
+          </button>
+        </>
       )}
     </div>
-
-
-
-    
   );
 };
 
-
-export default Pagination
+export default Pagination;
